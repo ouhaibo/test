bc9bbb450ceb3f1c4f5a22d974224c8c3030129a
ouhaibo <279811102@qq.com>
2017-08-12 00:03:24 +0800

    first

--- app/src/main/java/com/ohb/test/MainActivity.java
+++ app/src/main/java/com/ohb/test/MainActivity.java
@@ -1,16 +1,131 @@
 package com.ohb.test;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.app.ActivityThread;
+import android.app.Instrumentation;
+import android.content.ContentProvider;
+import android.content.Context;
+import android.content.Loader;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.hardware.input.InputManager;
+import android.os.Binder;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.IBinder;
+import android.os.IInterface;
+import android.os.Parcel;
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewRootImpl;
+
+import java.lang.ref.SoftReference;
+import java.lang.reflect.Field;
+import java.lang.reflect.GenericDeclaration;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import dalvik.system.DexClassLoader;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.tv0).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                hook();
+            }
+        });
+        findViewById(R.id.tv).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InputManager inputManager = (InputManager) getSystemService(Context.INPUT_SERVICE);
+                int[] ids = inputManager.getInputDeviceIds();
+                Log.d("ids:", ids[0] + "," + ids[1] + "," + ids[2] + "," + ids[3] + "," + ids[4] + ",");
+            }
+        });
+
+        findViewById(android.R.id.content).getViewTreeObserver().addOnGlobalLayoutListener(null);
+    }
+
+    private void hook_AMS() {
+        try {
+            Class<?> activityManagerNativeClass = Class.forName("android.app.ActivityManagerNative");
+            Field gDefaultField = activityManagerNativeClass.getDeclaredField("gDefault");
+            gDefaultField.setAccessible(true);
+            Object gDefault = gDefaultField.get(null);
+
+            Class<?> SingletonClass = Class.forName("android.util.Singleton");
+            Field mInstanceField = SingletonClass.getDeclaredField("mInstance");
+            mInstanceField.setAccessible(true);
+            Object originalIActivityManager = mInstanceField.get(gDefault);
+            Object hookedIActivityManager = Proxy.newProxyInstance(originalIActivityManager.getClass().getClassLoader(), new Class<?>[]{Class.forName("android.app.IActivityManager")}, new HookIActivityManagerHandler(originalIActivityManager));
+            mInstanceField.set(gDefault, hookedIActivityManager);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void hook_PMS_InOneContext() {
+        try {
+            Class<?> activityThreadClass = Class.forName("android.app.ActivityThread");
+            Field sPackageManagerField = activityThreadClass.getDeclaredField("sPackageManager");
+            sPackageManagerField.setAccessible(true);
+            Object originalIPackageManager = sPackageManagerField.get(null);
+
+            Object hookedIPackageManager = Proxy.newProxyInstance(originalIPackageManager.getClass().getClassLoader(), new Class<?>[]{IInterface.class, Class.forName("android.content.pm.IPackageManager")}, new HookIPackageManagerHandler(originalIPackageManager));
+            // 1. 替换掉ActivityThread里面的 sPackageManager 字段
+            Field sCurrentActivityThreadField = activityThreadClass.getDeclaredField("sCurrentActivityThread");
+            Object sCurrentActivityThread = sCurrentActivityThreadField.get(null);
+            sPackageManagerField.set(sCurrentActivityThread, hookedIPackageManager);
+            // 2. 替换 ApplicationPackageManager里面的 mPM对象
+            PackageManager pm = getPackageManager();
+            Field mPMField = pm.getClass().getDeclaredField("mPM");
+            mPMField.setAccessible(true);
+            mPMField.set(pm, hookedIPackageManager);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void hook() {
+        try {
+            Class<?> serviceManager = Class.forName("android.os.ServiceManager");
+            Method getService = serviceManager.getDeclaredMethod("getService", String.class);
+            IBinder rawInputManagerBinder = (IBinder) getService.invoke(null, Context.INPUT_SERVICE);
+
+            IBinder hookedProxyBinder = (IBinder) Proxy.newProxyInstance(serviceManager.getClassLoader(), new Class<?>[]{IBinder.class}, new BinderProxyHookHandler(rawInputManagerBinder));
+
+            Field sCache = serviceManager.getDeclaredField("sCache");
+            sCache.setAccessible(true);
+            HashMap<String, IBinder> map = (HashMap<String, IBinder>) sCache.get(null);
+            map.put(Context.INPUT_SERVICE, hookedProxyBinder);
+            sCache.set(null, map);
+            sCache.setAccessible(false);
+
+            Class<?> inputManagerClass = InputManager.class;
+            Field sInstance = inputManagerClass.getDeclaredField("sInstance");
+            sInstance.setAccessible(true);
+            sInstance.set(null, null);
+            sInstance.setAccessible(false);
+
+            InputManager inputManager = (InputManager) getSystemService(Context.INPUT_SERVICE);
+            Field mInputDevices = inputManagerClass.getDeclaredField("mInputDevices");
+            mInputDevices.setAccessible(true);
+            mInputDevices.set(inputManager, null);
+            mInputDevices.setAccessible(false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/main/java/com/ohb/test/com/ohb/test/pulltorefresh/RefreshableView.java
+++ app/src/main/java/com/ohb/test/com/ohb/test/pulltorefresh/RefreshableView.java
@@ -1,36 +1,271 @@
 package com.ohb.test.com.ohb.test.pulltorefresh;
 
 import android.content.Context;
+import android.os.AsyncTask;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.ohb.test.R;
 
 /**
  * Created by ouhaibo on 2015/10/6.
  */
-public class RefreshableView extends LinearLayout implements View.OnTouchListener{
+public class RefreshableView extends LinearLayout implements View.OnTouchListener {
+    public static final int STATUS_PULL_TO_REFRESH = 0;
+    public static final int STATUS_RELEASE_TO_REFRESH = 1;
+    public static final int STATUS_REFRESHING = 2;
+    public static final int STATUS_REFRESH_FINISHED = 3;
+    public static final int SCROLL_BACK_SPEED = 20;
+    private View mHeader;
+    private ListView mListView;
+    private ProgressBar mProgressBar;
+    private ImageView mImgArrow;
+    private TextView mTxtDesc;
+    private MarginLayoutParams headerLayoutParams;
+    private int id = -1;
+    private int hideHeaderHeight;
+    private int currentStatus = STATUS_REFRESH_FINISHED;
+    private int lastStatus = currentStatus;
+    private int touchSlop;
+    private float yDown;
+    private boolean loadOnce;
+    private boolean ableToPull;
+    private PullToRefreshListenr mListener;
+
     public RefreshableView(Context context) {
         super(context);
-        init();
+        init(context);
     }
 
     public RefreshableView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init();
+        init(context);
     }
 
     public RefreshableView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        init();
+        init(context);
     }
 
-    private void init(){
-        
+    private void init(Context context) {
+        mHeader = LayoutInflater.from(context).inflate(R.layout.pull_to_refresh_layout, null, true);
+        mProgressBar = (ProgressBar) mHeader.findViewById(R.id.progress_bar);
+        mImgArrow = (ImageView) mHeader.findViewById(R.id.arrow);
+        mTxtDesc = (TextView) mHeader.findViewById(R.id.description);
+        touchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+        setOrientation(VERTICAL);
+        addView(mHeader, 0);
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        super.onLayout(changed, l, t, r, b);
+        if (changed && !loadOnce) {
+            hideHeaderHeight = -mHeader.getHeight();
+            headerLayoutParams = (MarginLayoutParams) mHeader.getLayoutParams();
+            headerLayoutParams.topMargin = hideHeaderHeight;
+            mListView = (ListView) getChildAt(1);
+            mListView.setOnTouchListener(this);
+            loadOnce = true;
+
+        }
     }
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
+        setIsAbleToPull(event);
+        if (ableToPull) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    yDown = event.getRawY();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    float yMove = event.getRawY();
+                    int d = (int) (yMove - yDown);
+                    if (d <= 0 && headerLayoutParams.topMargin <= hideHeaderHeight) {
+                        return false;
+                    }
+                    if (d <= touchSlop) {
+                        return false;
+                    }
+                    if (currentStatus != STATUS_REFRESHING) {
+                        if (headerLayoutParams.topMargin <= 0) {
+                            currentStatus = STATUS_PULL_TO_REFRESH;
+                        } else {
+                            currentStatus = STATUS_RELEASE_TO_REFRESH;
+                        }
+                        headerLayoutParams.topMargin = (d / 2) + hideHeaderHeight;
+                        mHeader.setLayoutParams(headerLayoutParams);
+                    }
+                    break;
+                case MotionEvent.ACTION_UP:
+                default:
+                    if (currentStatus == STATUS_PULL_TO_REFRESH) {
+                        new HideHeaderTask().execute();
+                    } else if (currentStatus == STATUS_RELEASE_TO_REFRESH) {
+                        new RefreshingTask().execute();
+                    }
+                    break;
+            }
+            if (currentStatus == STATUS_PULL_TO_REFRESH || currentStatus == STATUS_RELEASE_TO_REFRESH) {
+                updateHeaderView();
+                mListView.setPressed(false);
+                mListView.setFocusable(false);
+                mListView.setFocusableInTouchMode(false);
+                lastStatus = currentStatus;
+                return true;
+            }
+
+        }
         return false;
+    }
+
+    private void updateHeaderView() {
+        if (lastStatus != currentStatus) {
+            if (currentStatus == STATUS_PULL_TO_REFRESH) {
+                mTxtDesc.setText(getResources().getString(R.string.pull_to_refresh));
+                mImgArrow.setVisibility(View.VISIBLE);
+                mProgressBar.setVisibility(View.GONE);
+                rotateArrow();
+            } else if (currentStatus == STATUS_RELEASE_TO_REFRESH) {
+                mTxtDesc.setText(getResources().getString(R.string.release_to_refresh));
+                mImgArrow.setVisibility(View.VISIBLE);
+                mProgressBar.setVisibility(View.GONE);
+                rotateArrow();
+            } else if (currentStatus == STATUS_REFRESHING) {
+                mTxtDesc.setText(getResources().getString(R.string.refreshing));
+                mProgressBar.setVisibility(View.VISIBLE);
+                mImgArrow.clearAnimation();
+                mImgArrow.setVisibility(View.GONE);
+            }
+        }
+    }
+
+    public void setOnFinishListener(int id, PullToRefreshListenr listenr) {
+        mListener = listenr;
+        this.id = id;
+    }
+
+
+    public void finishRefreshing() {
+        currentStatus = STATUS_REFRESH_FINISHED;
+        new HideHeaderTask().execute();
+    }
+
+
+    private void rotateArrow() {
+        float pivotX = mImgArrow.getWidth() / 2f;
+        float pivotY = mImgArrow.getHeight() / 2f;
+        float fromDegress = 0f;
+        float toDegress = 0f;
+        if (currentStatus == STATUS_PULL_TO_REFRESH) {
+            fromDegress = 180f;
+            toDegress = 360f;
+        } else if (currentStatus == STATUS_RELEASE_TO_REFRESH) {
+            fromDegress = 0f;
+            toDegress = 180f;
+        }
+        RotateAnimation anim = new RotateAnimation(fromDegress, toDegress, pivotX, pivotY);
+        anim.setDuration(500);
+        anim.setFillAfter(true);
+        mImgArrow.startAnimation(anim);
+    }
+
+    private void setIsAbleToPull(MotionEvent event) {
+        View firstChild = mListView.getChildAt(0);
+        if (firstChild != null) {
+            int firstVisiblePos = mListView.getFirstVisiblePosition();
+            if (firstVisiblePos == 0 && firstChild.getTop() == 0) {
+                if (!ableToPull) {
+                    yDown = event.getRawY();
+                }
+                ableToPull = true;
+            } else {
+                if (headerLayoutParams.topMargin != hideHeaderHeight) {
+                    headerLayoutParams.topMargin = hideHeaderHeight;
+                    mHeader.setLayoutParams(headerLayoutParams);
+                }
+                ableToPull = false;
+            }
+        } else {
+            ableToPull = true;
+        }
+    }
+
+    public interface PullToRefreshListenr {
+        void onRefresh();
+    }
+
+    class RefreshingTask extends AsyncTask<Void, Integer, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            int topMargin = headerLayoutParams.topMargin;
+            while (topMargin > 0) {
+                topMargin -= SCROLL_BACK_SPEED;
+                if (topMargin < 0) {
+                    topMargin = 0;
+                }
+                publishProgress(topMargin);
+            }
+            currentStatus = STATUS_REFRESHING;
+            if (mListener != null) {
+                mListener.onRefresh();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            headerLayoutParams.topMargin = values[0];
+            mHeader.setLayoutParams(headerLayoutParams);
+        }
+    }
+
+    class HideHeaderTask extends AsyncTask<Void, Integer, Integer> {
+
+        @Override
+        protected Integer doInBackground(Void... params) {
+            int topMargin = headerLayoutParams.topMargin;
+            while (true) {
+                topMargin -= SCROLL_BACK_SPEED;
+                if (topMargin <= hideHeaderHeight) {
+                    topMargin = hideHeaderHeight;
+                    break;
+                }
+                publishProgress(topMargin);
+            }
+            return topMargin;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... topMargin) {
+            headerLayoutParams.topMargin = topMargin[0];
+            mHeader.setLayoutParams(headerLayoutParams);
+        }
+
+        @Override
+        protected void onPostExecute(Integer topMargin) {
+            headerLayoutParams.topMargin = topMargin;
+            mHeader.setLayoutParams(headerLayoutParams);
+            currentStatus = STATUS_REFRESH_FINISHED;
+        }
+    }
+
+    private void sleep(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
